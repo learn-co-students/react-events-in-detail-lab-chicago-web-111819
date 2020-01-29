e3e193f202bf0c3d21b1973342f532ffa45f0cd8
dut-a <d.dut@live.com>
2020-01-28 22:01:01 -0600

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -7,7 +7,7 @@ export default class DelayedButton extends Component {
   delayedBtn = evt => {
     // hold onto event to use it after 'setTimeout()'
     evt.persist();
-    window.setTimeout(() => this.props.onDelayedClick(evt), this.props.delay);
+    window.setTimeout((() => this.props.onDelayedClick(evt)), this.props.delay);
   }
 
   render() {
